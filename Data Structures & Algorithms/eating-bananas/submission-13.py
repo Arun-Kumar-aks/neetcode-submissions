59eb9971d456758ede1b43f729fd02b2e78519cd
Arun Kumar S <s.arunkumar7305@gmail.com>
2026-09-11 13:19:12 +0530

    Add: eating-bananas - submission-13

---
+++ Data Structures & Algorithms/eating-bananas/submission-13.py
@@ -0,0 +1,25 @@
+class Solution:
+    def is_eat(self,piles,h,m):
+        count=h
+        print(m)
+        for i in piles:
+            count-=(i//m)+1
+            if (i//m)==(i/m):
+                count+=1
+        print('m',m,' ',count)
+        if count>=0:
+            return True
+        return False 
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        result=0
+        l=1
+        r=sum(piles)
+        while l<=r:
+            m=(l+r)//2
+            if self.is_eat(piles,h,m):
+                r=m-1
+                result=m
+            else:
+                l=m+1
+        return result
