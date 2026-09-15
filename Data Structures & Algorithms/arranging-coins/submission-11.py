3f1c0f63de04cbbdc86228e1d8ed36b36846bdac
Arun Kumar S <s.arunkumar7305@gmail.com>
2026-09-15 20:52:40 +0530

    Add: arranging-coins - submission-11

---
+++ Data Structures & Algorithms/arranging-coins/submission-11.py
@@ -0,0 +1,17 @@
+class Solution:
+    def is_arrange(self,m,n):
+        k=m*(m+1)/2
+        return k<=n
+
+    def arrangeCoins(self, n: int) -> int:
+        l=0
+        r=n
+        result=0
+        while l<=r:
+            m=(l+r)//2
+            if self.is_arrange(m,n):
+                result=m
+                l=m+1
+            else:
+                r=m-1    
+        return result
